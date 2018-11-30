99f12777cd99fa566a437fcee3fbd39388543d5c
Gabriel Gonzalez <gaabgonca@gmail.com>
2018-11-30 18:06:04 -0500

    MVP finshed

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const final_date = new Date(2022, 7, 7);
+    const today = new Date();
+    const days = Date.daysBetween(final_date, today);
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <p>Quedan</p>
+          <h1>{days}</h1>
+          <p>días del gobierno de Iván Duque.</p>
         </header>
       </div>
     );
   }
 }
+
+Date.daysBetween = function(date1, date2) {
+  var one_day = 1000 * 60 * 60 * 24; //Dia en milisegundos
+  var date1_ms = date1.getTime();
+  var date2_ms = date2.getTime();
+  var difference_ms = date1_ms - date2_ms;
+  return Math.round(difference_ms / one_day);
+};
 
 export default App;
