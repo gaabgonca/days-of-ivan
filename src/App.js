ecad98186f01d99a01c32d2b2a4f0af54ae4f40b
Gabriel Gonzalez <gaabgonca@gmail.com>
2018-11-30 19:11:16 -0500

    Added share buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
 import React, { Component } from "react";
 import "./App.css";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappShareButton,
+  WhatsappIcon
+} from "react-share";
 
 class App extends Component {
   render() {
     const final_date = new Date(2022, 7, 7);
     const today = new Date();
     const days = Date.daysBetween(final_date, today);
+    const url = "days-of-ivan.herokuapp.com";
     return (
       <div className="App">
         <header className="App-header">
+          <div>
+            <p className="share-text">
+              Comparte esto con tus familiares, los que se enhuesaron con su
+              voto.
+            </p>
+            <div className="share-buttons">
+              <FacebookShareButton
+                className="share-button"
+                url={url}
+                quote={"¿Cuantos días le quedan a Iván Duque? "}
+              >
+                <FacebookIcon size={32} round />
+              </FacebookShareButton>
+              <TwitterShareButton
+                className="share-button"
+                url={url}
+                title={"¿Cuantos días le quedan a Iván Duque? " + url}
+              >
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+              <WhatsappShareButton className="share-button" url={url}>
+                <WhatsappIcon size={32} round />
+              </WhatsappShareButton>
+            </div>
+          </div>
           <p>Quedan</p>
           <h1>{days}</h1>
           <p>días del gobierno de Iván Duque.</p>
